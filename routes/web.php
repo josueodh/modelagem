8328c933f0d5ad2c30085f456a0f839466e4dd75
josue heringer <josuedelgadoheringer98@gmail.com>
2019-09-09 23:07:41 -0300

    xd

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,3 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::resource('/members', 'MemberController');
-Route::resource('/roles', 'RoleController');
-Route::resource('/project', 'ProjectController');
